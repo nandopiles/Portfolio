392dd72f0b622b87a75391026e208f554383de98
Ferran Piles <ferran.piles@conmuta.es>
2025-09-30 13:16:26 +0200

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
+import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavigationComponent,
+    HeroSectionComponent,
+    AboutSectionComponent,
+    SkillsSectionComponent,
+    ProjectsSectionComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
