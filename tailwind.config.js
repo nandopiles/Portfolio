92955aed82061ab4f48e529f354d69105a16258e
Ferran Piles <ferran.piles@conmuta.es>
2025-09-30 13:15:44 +0200

    public

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Incluye todos los archivos HTML y TS de Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
