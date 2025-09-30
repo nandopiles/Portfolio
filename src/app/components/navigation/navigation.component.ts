a22af7ff92b460db0d3d63fd9b6115583fbdd7e1
Ferran Piles <ferran.piles@conmuta.es>
2025-09-30 13:35:56 +0200

    fix navigation and routes added

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 interface NavItem {
